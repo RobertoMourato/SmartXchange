2a21c945f0e1b7d0837f9ea2c7d749b3b6d8c3b9
RobertoMourato <up201705616@fe.up.pt>
2020-10-20 14:25:08 +0100

    changed PORT management

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var express = require("express");
 var app = express();
 
-const PORT = 8000;
-
-app.listen(PORT, () => {
-    console.log("Server running on PORT " + PORT + "...");
-})
-
 app.get("/", (req, res) => {
     res.send("Hello World!");
 })
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, function() {
+  console.log(`App listening on port ${PORT}`);
+});
