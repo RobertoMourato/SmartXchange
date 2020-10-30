f749170430beaf3af5e8105d455aef4a1ce2c3b1
LVStein <lucasvieirastein@gmail.com>
2020-10-30 23:53:56 +0000

    [#67] <Feature> CREATED MIGRATIONS AND MODELS FOR USER AND USERTYPE
    Added migration and model files to create the User and UserId tables on the database.

---
+++ migrations/20201030233500-create-user.js
@@ -0,0 +1,60 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      tenantId: {
+        references:{
+          model: {
+            tableName: 'tenants'
+          },
+          key: 'id'
+        },
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      userTypeId: {
+        references:{
+          model: {
+            tableName: 'usertypes'
+          },
+          key: 'id'
+        },
+        type: Sequelize.INTEGER
+      },
+      initialBudget: {
+        type: Sequelize.INTEGER
+      },
+      currentBalance: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
---
+++ models/usertype.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class UserType extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.hasMany(models.User, {foreignKey:'userTypeId'});
+    }
+  };
+  UserType.init({
+    userTypeId: DataTypes.INTEGER,
+    userType: DataTypes.STRING,
+    isEntrepreneur: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'UserType',
+  });
+  return UserType;
+};
