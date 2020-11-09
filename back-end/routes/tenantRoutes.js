81ee2c61abb53cbed9ad4c96f11a96abc55b29db
MrFurlan98 <viniciusrf1998@hotmail.com>
2020-11-09 22:51:23 +0000

    Revert "Merge branch '65-create-default-nav' into 'master'"
    This reverts merge request !12

--- back-end/routes/tenantRoutes.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-var tenantController = require('../controllers/tenantController'); 
-
-router.get('/',tenantController.getAllTenants);
-router.post('/',tenantController.addTenant);
-
-module.exports = router;
--- back-end/routes/tenantTypeRoutes.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-
-var tenantController = require('../controllers/tenantController'); 
-
-router.get('/',tenantController.getAllTenantTypes);
-
-module.exports = router;
