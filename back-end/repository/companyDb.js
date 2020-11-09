9b39cd2a3c2299093165da4b3909fa55cf3b06a7
LVStein <lucasvieirastein@gmail.com>
2020-11-09 10:37:29 +0000

    [70] <Feature> ADDED FUNCTION THAT HANDLES POST REQUEST
    Created the function that handles the post request for one company.

--- back-end/repository/companyDb.js
+++ back-end/repository/companyDb.js
@@ -4,23 +4,41 @@ const models = require('../models');
 module.exports = {
 
   async addCompany(req, res) {
-    //console.log(req.body)
 
-    /*const userType = await models.UserType.findOne({ where: { userType: req.body.userType } });
-    const { name, username, email, password, tenantId } = req.body;
-
-    if (userType) {
+    const { playerCompetitionId,
+            companyName,
+            companyValuePropositions, 
+            companyCostumerSegments, 
+            companyCostumerRelationships, 
+            companyChannels, 
+            companyCostStructure, 
+            companyKeyPartners, 
+            companyKeyResources, 
+            companyWebsiteURL, 
+            companyShortPitch, 
+            companyCurrentStockPrice } = req.body;
+    
+    if (companyName) {
       try {
-        const typeId = userType.dataValues.id;
-        console.log(typeId)
-        const user = await models.User.create({ name:name, username:username, email:email, password:password, tenantId:tenantId, userTypeId:typeId });
-        res.status(200).json(user)
+        const company = await models.Company.create({ playerCompetitionId:playerCompetitionId, 
+                                                      companyName:companyName, 
+                                                      companyValuePropositions:companyValuePropositions, 
+                                                      companyCostumerSegments:companyCostumerSegments, 
+                                                      companyCostumerRelationships:companyCostumerRelationships, 
+                                                      companyChannels:companyChannels, 
+                                                      companyCostStructure:companyCostStructure, 
+                                                      companyKeyPartners:companyKeyPartners, 
+                                                      companyKeyResources:companyKeyResources, 
+                                                      companyWebsiteURL:companyWebsiteURL, 
+                                                      companyShortPitch:companyShortPitch, 
+                                                      companyCurrentStockPrice:companyCurrentStockPrice });
+        res.status(200).json(company);
       } catch (error) {
         res.status(400).json(error);
       }
     } else {
-      res.status(400).json("No User Type associated");
-    }*/
+      res.status(400).json("No company name associated");
+    }
 
   }
 }
