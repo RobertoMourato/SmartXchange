e53c90c2105b4e499ee3469b2b53a886eaa1fb43
Vinicius Ribeiro Furlan <mr.furlan98@gmail.com>
2020-11-09 22:31:55 +0000

    [#68] <Fix> CHANGED ROUTE NAME
    Changed route name from start to toggle

--- routes/competitionRoutes.js
+++ routes/competitionRoutes.js
@@ -5,7 +5,7 @@ var competitionController = require('../controllers/competitionController');
 
 router.get('/',competitionController.getAllCompetitions);
 router.post('/',competitionController.addCompetition);
-router.put('/start',competitionController.toggleCompetition);
+router.put('/toggle',competitionController.toggleCompetition);
 router.put('/settings',competitionController.changeSettingsCompetition);
 
 module.exports = router;
