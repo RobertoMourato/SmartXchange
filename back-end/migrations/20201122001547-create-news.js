8f383e163e029c0985d148d3f5eaf64798ba6f63
LVStein <lucasvieirastein@gmail.com>
2020-11-22 00:17:35 +0000

    [#84] <Feature> ADDED MIGRATION AND MODEL FILES FOR NEWS
    Created the migration and model files for the news table so that it is easy to create this table in the database.

---
+++ back-end/migrations/20201122001547-create-news.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('News', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      competitionId: {
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER
+      },
+      newsTitle: {
+        type: Sequelize.STRING
+      },
+      newsContent: {
+        type: Sequelize.STRING
+      },
+      newsType: {
+        type: Sequelize.STRING
+      },
+      newsDate: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('News');
+  }
+};
