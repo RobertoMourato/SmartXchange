60ace9d985980d7a2033aa592983620f93d24296
LVStein <lucasvieirastein@gmail.com>
2020-11-24 18:33:57 +0000

    [#84] <Fix> CORRECTED NEWS TABLE ASSOCIATION
    Added the right associations to the News table which was not done correctly before.

--- back-end/models/news.js
+++ back-end/models/news.js
@@ -10,7 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      this.belongsTo(models.Competition, {foreignKey:'competitionId'});
+      this.belongsTo(models.User, {foreignKey:'userId'});
     }
   };
   News.init({
