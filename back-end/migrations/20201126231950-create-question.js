8d88341e1405111415e5194a4b2a9fc6a50ed87a
Murilo Couceiro <up202003377@fe.up.pt>
2020-11-27 00:35:40 +0000

    [#88] <Feature> ADDED QUESTION MODEL AND MIGRATION
    Added the migration and model for the table Question. Also added the associations related to this

---
+++ back-end/migrations/20201126231950-create-question.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Questions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      questionText: {
+        type: Sequelize.STRING
+      },
+      order: {
+        type: Sequelize.INTEGER
+      },
+      competitionId: {
+        type: Sequelize.INTEGER,
+        references:{
+          model: {
+            tableName: 'competitions'
+          },
+          key: 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Questions');
+  }
+};
