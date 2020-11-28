519f6f859ddc8e27f470aeb333dde5016292f0a0
Murilo Couceiro <up202003377@fe.up.pt>
2020-11-28 01:25:59 +0000

    [#89] <Feature> CREATED MIGRATION AND ASSOCIATIONS
    Added the migration and model files. Created associations with question and company

--- back-end/models/question.js
+++ back-end/models/question.js
@@ -12,6 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       this.belongsTo(models.Competition, {foreignKey:'competitionId', as:'competition'})
+      this.hasMany(models.Answer, {foreignKey: 'questionId', as: 'responses'})
     }
   };
   Question.init({
