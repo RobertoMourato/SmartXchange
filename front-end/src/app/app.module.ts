2ca2f2daf7f31b7ae97b230a92a9d549ac74ca6d
atomatir <atomnatir@gmail.com>
2020-12-03 00:20:51 +0000

    [#74] <Feature> Started from for competition creation

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,30 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SuperadminNavComponent } from './superadmin-nav/superadmin-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+import { SuperadminNavComponent } from './superadmin-nav/superadmin-nav.component';
+import { NewCompetitionComponent } from './new-competition/new-competition.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SuperadminNavComponent
+    SuperadminNavComponent,
+    NewCompetitionComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     LayoutModule,
+    FormsModule,
+    ReactiveFormsModule,
     MatToolbarModule,
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
