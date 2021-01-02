3636a1bb5b7ee95506a2efb6003c24dccfa5158e
Vinicius Ribeiro Furlan <mr.furlan98@gmail.com>
2021-01-02 16:03:09 +0000

    [#98] <Feature> ADDED MARKET LIST SERVICE FILE

---
+++ front-end/src/app/market-page-list/market-page-list.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient , HttpHeaders} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MarketPageListService {
+
+  private url = 'http://localhost:3000';
+  // private url = 'http://localhost:5000'
+
+  constructor(private httpClient: HttpClient) { }
+  
+}
